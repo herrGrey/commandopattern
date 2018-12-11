f3ec865041a22e7db6afcdabec5dc8ae7bde486e
Stefan Seibl <s.seibl34@gmail.com>
2018-12-11 17:10:19 +0100

    junit test

---
+++ src/test/Befehl/RechnungerstellenTest.java
@@ -0,0 +1,60 @@
+package test.Befehl; 
+
+import Empfaenger.Kassensystem;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After;
+import sample.Rechnung;
+import org.hamcrest.collection.IsEmptyCollection;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+        import static org.hamcrest.Matchers.hasProperty;
+        import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+
+
+import static org.junit.Assert.*;
+
+/** 
+* Rechnungerstellen Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Dez 11, 2018</pre> 
+* @version 1.0 
+*/ 
+public class RechnungerstellenTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: Rechnungerstellen(String tisch) 
+* 
+*/ 
+@Test
+public void testRechnungerstellen() throws Exception { 
+   Kassensystem kassensystem = new Kassensystem();
+   assertThat(kassensystem.Rechnungdrucken("21"), containsInAnyOrder(
+           hasProperty("gericht", is("Pizza")),
+           hasProperty("gericht", is("Burger")),
+           hasProperty("gericht", is("Burger"))
+   ));
+   assertThat(kassensystem.Rechnungdrucken("21"),hasSize(3));
+
+}
+
+
+} 
---
+++ src/test/sample/ControllerTest.java
@@ -0,0 +1,109 @@
+package test.sample; 
+
+import Aufrufer.Kellner;
+import Befehl.Bestellvorgang;
+import Befehl.RechnungBefehl;
+import Befehl.Rechnungerstellen;
+import Empfaenger.Kassensystem;
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After;
+import static org.junit.Assert.*;
+
+import sample.Controller;
+import sample.Rechnung;
+import sample.Singelton;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/** 
+* Controller Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Dez 11, 2018</pre> 
+* @version 1.0 
+*/ 
+public class ControllerTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: gerichteLaden() 
+* 
+*/ 
+@Test
+public void testGerichteLaden() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: tischeLaden() 
+* 
+*/ 
+@Test
+public void testTischeLaden() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: Rechnungdrucken() 
+* 
+*/ 
+@Test
+public void testRechnungdrucken() throws Exception {
+
+
+} 
+
+/** 
+* 
+* Method: Bestellen() 
+* 
+*/ 
+@Test
+public void testBestellen() throws Exception {
+
+} 
+
+/** 
+* 
+* Method: Stornieren() 
+* 
+*/ 
+@Test
+public void testStornieren() throws Exception { 
+
+} 
+
+/** 
+* 
+* Method: getUmsatz() 
+* 
+*/ 
+@Test
+public void testGetUmsatz() throws Exception { 
+    try {
+        Statement statement = Singelton.getConn().createStatement();
+        ResultSet set = statement.executeQuery("SELECT SUM(tbl_gericht.g_preis)AS Preis\n" +
+                "                    FROM tbl_rechnung\n" +
+                "                    JOIN tbl_gericht ON tbl_rechnung.tbl_gericht_g_id=tbl_gericht.g_id");
+
+        assertTrue(set.next());
+        assertTrue(57==set.getInt("Preis"));
+    }catch (Exception exc){
+        exc.printStackTrace();
+    }
+}
+
+
+} 
